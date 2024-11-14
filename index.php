5a2bc3324087c4c348e006a46985982de8e67a5b
John Cris <johncrismanabo9@gmail.com>
2024-11-14 09:56:20 +0800

    dynamic maintenance area description

--- index.php
+++ index.php
@@ -1,33 +1,89 @@
 <?php
-include 'script.php';
+include 'script.php'; 
 
-// get maintenance criteria setup
+$data = []; 
+$maintenance_area_description_query = "SELECT * FROM `maintenance_area_description`";
+$maintenance_area_description_result = mysqli_query($conn, $maintenance_area_description_query);
 
-$maintenance_criteria_setup = "
-    SELECT
-        msc.keyctr AS keyctr,
-        mai.indicator_description indicator,
-        mai.relevance_def relevance_definition,
-        mam.description,
-        msc.movdocs_reqs documentary_requirements,
-        mds.srcdesc data_source
-    FROM
-        `maintenance_criteria_setup` msc
-    left JOIN maintenance_criteria_version AS mcv
-    ON
-        msc.version_keyctr = mcv.keyctr
-    left  JOIN maintenance_area_indicators AS mai
-    ON
-        msc.indicator_keyctr = mai.keyctr
-    left JOIN maintenance_area_mininumreqs AS mam
-    ON
-        msc.minreqs_keyctr = mam.keyctr
-    LEFT JOIN maintenance_document_source AS mds
-    ON
-        msc.data_source = mds.keyctr order by msc.keyctr  desc
-";
-$maintenance_criteria_setup_result = mysqli_query($conn, $maintenance_criteria_setup);
 
+
+if ($maintenance_area_description_result && mysqli_num_rows($maintenance_area_description_result) > 0) {
+    // Fetch and display each row with var_dump
+    while ($maintenance_area_description_row = mysqli_fetch_assoc($maintenance_area_description_result)) {
+
+        // maintenance_governance
+        $maintenance_governance_query = "SELECT * FROM `maintenance_governance` where desc_keyctr = '$maintenance_area_description_row[keyctr]'";
+        $maintenance_governance_result = mysqli_query($conn, $maintenance_governance_query);
+
+        if ($maintenance_governance_result && mysqli_num_rows($maintenance_governance_result) > 0) {
+            while ($maintenance_governance_row = mysqli_fetch_assoc($maintenance_governance_result)) {
+
+                $query2_data = $maintenance_governance_row;
+
+                // maintenance_area_indicators
+                $maintenance_area_indicators_query = "SELECT * FROM `maintenance_area_indicators` where governance_code = '$maintenance_governance_row[keyctr]'";
+                $maintenance_area_indicators_result = mysqli_query($conn, $maintenance_area_indicators_query);
+
+
+
+                if ($maintenance_area_indicators_result && mysqli_num_rows($maintenance_area_indicators_result) > 0) {
+                    while ($maintenance_area_indicators_row = mysqli_fetch_assoc($maintenance_area_indicators_result)) {
+
+                        $query3_data = $maintenance_area_indicators_row;
+
+
+                        // maintenance_criteria_setup
+                        $maintenance_criteria_setup_query = "
+                            SELECT 
+                            
+                                msc.keyctr AS keyctr,
+                                mam.description,
+                                msc.movdocs_reqs documentary_requirements,
+                                mds.srcdesc data_source
+
+
+                            FROM `maintenance_criteria_setup` msc 
+                            left JOIN maintenance_criteria_version AS mcv
+                            ON
+                                msc.version_keyctr = mcv.keyctr
+                            left JOIN maintenance_area_mininumreqs AS mam
+                            ON
+                                msc.minreqs_keyctr = mam.keyctr
+                            LEFT JOIN maintenance_document_source AS mds
+                            ON
+                                msc.data_source = mds.keyctr 
+                            
+                            where msc.indicator_keyctr  = '$maintenance_area_indicators_row[keyctr]'";
+                        $maintenance_criteria_setup_result = mysqli_query($conn, $maintenance_criteria_setup_query);
+
+
+                        if ($maintenance_criteria_setup_result && mysqli_num_rows($maintenance_criteria_setup_result) > 0) {
+                            while ($maintenance_criteria_setup_row = mysqli_fetch_assoc($maintenance_criteria_setup_result)) {
+
+                                $data[$maintenance_area_description_row['description']][] = array(
+                                    'keyctr' => $maintenance_criteria_setup_row['keyctr'],
+                                    'indicator_description' => $maintenance_area_indicators_row['indicator_description'],
+                                    'relevance_definition' => $maintenance_area_indicators_row['relevance_def'],
+                                    'documentary_requirements' => $maintenance_criteria_setup_row['documentary_requirements'],
+                                    'description' => $maintenance_criteria_setup_row['description'],
+                                    'data_source' => $maintenance_criteria_setup_row['data_source'],
+
+
+                                );
+
+                            }
+                        }
+
+
+                    }
+                }
+
+
+            }
+        } 
+
+    }
+} 
 ?>
 
 
@@ -52,42 +108,59 @@ $maintenance_criteria_setup_result = mysqli_query($conn, $maintenance_criteria_s
                 </div>
             </div>
         </div>
-        <div class="text-center">
-            <h5>
-                CORE GOVERNANCE AREA 1: FINANCIAL ADMINISTRATION AND SUSTAINABILITY
-            </h5>
-        </div>
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>Action</th>
-                    <th>Indicator</th>
-                    <th>Relevant/Definition </th>
-                    <th>Minimum Requirements</th>
-                    <th>Documentary Requirements/MOVs</th>
-                    <th>Data Source</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                if (mysqli_num_rows($maintenance_criteria_setup_result) > 0) {
-                    while ($row = mysqli_fetch_assoc($maintenance_criteria_setup_result)) { ?>
-                        <tr>
-                            <td>
-                                <a href="edit.php?edit_id=<?php echo $row['keyctr'] ?>">Edit</a> |
-                                <a href="script.php?delete_id=<?php echo $row['keyctr'] ?>">Delete</a>
-                            </td>
-                            <td><?php echo $row['indicator']; ?></td>
-                            <td><?php echo $row['relevance_definition']; ?></td>
-                            <td><?php echo $row['description']; ?></td>
-                            <td><?php echo $row['documentary_requirements']; ?></td>
-                            <td><?php echo $row['data_source']; ?></td>
-                        </tr>
-                    <?php }
-                }
+        <?php
+
+        foreach ($data as $key => $rows) { ?>
+            <div class="text-center">
+                <h5>
+                    <?php echo $key; ?>
+                </h5>
+            </div>
+
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Action</th>
+                        <th>Indicator</th>
+                        <th>Relevant/Definition </th>
+                        <th>Minimum Requirements</th>
+                        <th>Documentary Requirements/MOVs</th>
+                        <th>Data Source</th>
+                    </tr>
+                </thead>
+                <tbody>
+        <?php
+            foreach ($rows as $row) { 
+            
                 ?>
-            </tbody>
-        </table>
+                <tr>
+                    <td>
+                        <a href="edit.php?edit_id=<?php echo $row['keyctr'] ?>">Edit</a> |
+                        <a href="script.php?delete_id=<?php echo $row['keyctr'] ?>">Delete</a>
+                    </td>
+                    <td><?php echo $row['indicator_description']; ?></td>
+                    <td><?php echo $row['relevance_definition']; ?></td>
+                    <td><?php echo $row['description']; ?></td>
+                    <td><?php echo $row['documentary_requirements']; ?></td>
+                    <td><?php echo $row['data_source']; ?></td>
+                </tr>
+
+                <?php
+                    } ?>
+    
+
+
+                </tbody>
+            </table> 
+
+            <br />
+            <br />
+
+            
+            <?php
+                    }  
+
+        ?>
 
     </div>
 </body>
